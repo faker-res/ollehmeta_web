92dcb0c7bd5a45dbc8fa64f1b3ec32e22f309986
ChoiYonghun <powerhango@gmail.com>
2019-12-21 20:27:27 +0900

    - MCID팝업 리스트 최종만 아니라 모두 나오도록  수정.

--- src/main/webapp/resources/js/metatag.js
+++ src/main/webapp/resources/js/metatag.js
@@ -1784,18 +1784,23 @@ function btnMcidSearch(){
         	var listItems = response.RESULT.LIST_ITEMS;
         	for(var idx in response.RESULT.LIST_ITEMS){
         		//if(MetaPopupInstance.movieInfo.RESULT.CID==listItems[idx].CID){
-        		if(MetaPopupInstance.cidPop==listItems[idx].CID){//2019.12.10
+        		if(MetaPopupInstance.cidPop==listItems[idx].CID && 'Y'==listItems[idx].STAT){//2019.12.10
         			strFontRed = 'style="color:red;"';
         		}else{
         			strFontRed = "";
         		}
-        		
+
+                //태그정보 로딩 getTagsFromMcidSearchResult
+                strHtml += 	'<tr>'+
+                    '	<td '+strFontRed+'>'+listItems[idx].CID+'</td>'+
+                    '	<td '+strFontRed+'>'+listItems[idx].TITLE+'('+listItems[idx].ITEMID+')</td>'+
+                    '</tr>';
         		
         		//태그정보 로딩 getTagsFromMcidSearchResult
-        		strHtml += 	'<tr onclick="getTagsFromMcidSearchResult('+listItems[idx].ITEMID+',\''+listItems[idx].STAT+'\','+listItems[idx].CNT_TAG + ',\''+listItems[idx].CID+'\')" style="cursor:pointer">'+
-							'	<td '+strFontRed+'>'+listItems[idx].CID+'</td>'+
-							'	<td '+strFontRed+'>'+listItems[idx].TITLE+'</td>'+
-							'</tr>';
+        		// strHtml += 	'<tr onclick="getTagsFromMcidSearchResult('+listItems[idx].ITEMID+',\''+listItems[idx].STAT+'\','+listItems[idx].CNT_TAG + ',\''+listItems[idx].CID+'\')" style="cursor:pointer">'+
+				// 			'	<td '+strFontRed+'>'+listItems[idx].CID+'</td>'+
+				// 			'	<td '+strFontRed+'>'+listItems[idx].TITLE+'</td>'+
+				// 			'</tr>';
         	}
         	$("#tblMcidSearchResult > tbody").append(strHtml);
         	
